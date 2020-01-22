463a6e53167d8beb772b0b8a530be6eb9457de92
Shriyam Tripathi <tripathishriyam99@gmail.com>
2020-01-22 14:13:46 +0530

    added

--- playgrounds/mongodb-delete.js
+++ playgrounds/mongodb-delete.js
@@ -2,10 +2,29 @@ const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017', (err, client)=>{
   var db = client.db('TodoApp');
-  db.collection('todos').find({completed: false}).toArray().then((data)=>{
-    console.log(data);
-  }).catch((err)=>{
-    console.log(err);
+  db.collection('Todos').findOneAndDelete({completed: false}).then((res)=>{
+    console.log(res);
   })
   client.close();
 })
+
+// MongoClient.connect('mongodb://localhost:27017', (err, client)=>{
+//   var db = client.db('TodoApp');
+//   db.collection('Todos').insertMany([
+//     {
+//       task: 'coding',
+//       completed: true
+//     },
+//     {
+//       task: 'eat food',
+//       completed: true
+//     },
+//     {
+//       task: 'dance practice',
+//       completed: false
+//     }
+//   ], (err, res)=>{
+//     console.log(res.ops);
+//   });
+//   client.close();
+// });
