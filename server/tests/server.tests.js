54c62a12af24f3e6000af0cafc7d043c470e9cb1
Shriyam Tripathi <tripathishriyam99@gmail.com>
2020-01-26 02:15:42 +0530

    added delete test script

--- server/tests/server.tests.js
+++ server/tests/server.tests.js
@@ -95,9 +95,41 @@ describe('GET todos/:id', ()=>{
   it('should return 404 id incorrect', (done)=>{
     request(app)
     .get(`/todos/e2b63fec457ae298886d4a3`)
+    .expect(400)
+    .expect((res)=>{
+      expect(res.body.errorMessage).toBe('Id incorrect');
+    })
+    .end(done);
+  });
+});
+
+describe('DELETE /todos/:id', ()=>{
+  it('should delete a todo', (done)=>{
+    request(app)
+    .delete(`/todos/${loadTodoData[0]._id}`)
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todo.text).toBe(loadTodoData[0].text);
+    })
+    .end(done);
+  });
+
+  it('should return 404 if todo not found', (done)=>{
+    request(app)
+    .delete('/todos/123234')
     .expect(404)
     .expect((res)=>{
       expect(res.body.errorMessage).toBe('Id incorrect');
+    })
+    .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done)=>{
+    request(app)
+    .delete('/todos/6e2b6669c908bb04f47a186a')
+    .expect(404)
+    .expect((res)=>{
+      expect(res.body.errorMessage).toBe('Data not found');
     })
     .end(done);
   });
