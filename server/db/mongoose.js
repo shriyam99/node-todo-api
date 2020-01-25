0101b84555fe4b2aa9547764a0c4a6b47e928d80
Shriyam Tripathi <tripathishriyam99@gmail.com>
2020-01-25 23:42:09 +0530

    made some changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');   //importing mongoose
+const dbURL = process.env.MONGODB_URL || 'mongodb://localhost:27017/TodoApp';
 
 mongoose.Promise = global.Promise;      //to set the promise of global
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true, useUnifiedTopology: true});   //connecting to db
-// mongoose.connect('mongodb+srv://shriyam:herokusample@cluster0-k8dbj.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});   //connecting to db
+mongoose.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true});   //connecting to db
 
 module.exports = {mongoose};
